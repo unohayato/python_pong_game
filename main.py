a4351a1089759b5b8d366a351a344c5a4749a51d
unohayato <mamidori1p1p@gmail.com>
2022-09-12 23:47:27 +0900

    set

---
+++ main.py
@@ -0,0 +1,10 @@
+from http.client import ImproperConnectionState
+
+
+import turtle
+
+wn = turtle.Screen()
+wn.title("Pong Game")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
