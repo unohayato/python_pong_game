77bc6396c437c9e4a3e13222077b5ec31ca7f839
unohayato <mamidori1p1p@gmail.com>
2022-09-13 12:13:36 +0900

    paddle_a_up()

--- main.py
+++ main.py
@@ -35,6 +35,15 @@ ball.color("white")
 ball.penup()
 ball.goto(0, 0)
 
+#Function
+def paddle_a_up():
+  y = paddle_a.ycor()
+  y += 20
+  paddle_a.sety(y)
+
+#Keyboard binding
+wn.listen()
+wn.onkeypress(paddle_a_up, "w")
 
 while True:
   wn.update()
