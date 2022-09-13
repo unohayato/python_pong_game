05302402ec2043becc4efcfd972d268d6648b88e
unohayato <mamidori1p1p@gmail.com>
2022-09-13 12:06:02 +0900

    Ball

--- main.py
+++ main.py
@@ -27,5 +27,14 @@ paddle_b.penup()
 paddle_b.goto(350, 0)
 
 
+#Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+
+
 while True:
   wn.update()
