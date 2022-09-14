1d3bee3fff295621f711f492078bafe6de84c41f
unohayato <mamidori1p1p@gmail.com>
2022-09-14 11:18:54 +0900

    paddle and ball collisions 2

--- main.py
+++ main.py
@@ -89,6 +89,7 @@ while True:
     ball.dx *= -1
     
   #Paddle and ball collisions
-  if ball.xcor() > 340 and (ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50):
+  if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50):
     ball.dx *= -1 
+  if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50): ball.dx *= -1 
     
