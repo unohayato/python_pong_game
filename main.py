e215a90397a134dfa999f3cd40ff7adc6775d9b3
unohayato <mamidori1p1p@gmail.com>
2022-09-14 11:00:06 +0900

    ball rebound y

--- main.py
+++ main.py
@@ -71,3 +71,8 @@ while True:
   #Move the ball
   ball.setx(ball.xcor() + ball.dx)
   ball.sety(ball.ycor() + ball.dy)
+  
+  #Ball checking
+  if ball.ycor() > 290:
+    ball.sety(290)
+    ball.dy *= -1
