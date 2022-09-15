13d0964a0fc976d67a12d69c60cf8993dc4edaeb
unohayato <mamidori1p1p@gmail.com>
2022-09-15 11:07:46 +0900

    score inclement

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 
-from logging import PercentStyle
 import turtle
 
 wn = turtle.Screen()
@@ -7,6 +6,10 @@ wn.title("Pong Game")
 wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
+
+#Score
+score_a = 0
+score_b = 0
 
 #Paddle A
 paddle_a = turtle.Turtle()
@@ -98,9 +101,17 @@ while True:
   if ball.xcor() > 390:
     ball.goto(0, 0)
     ball.dx *= -1
+    score_a += 1
+    pen.clear()
+    pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+
   if ball.xcor() < -390:
     ball.goto(0, 0)
     ball.dx *= -1
+    score_b += 1
+    pen.clear()
+    pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+
     
   #Paddle and ball collisions
   if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50):
